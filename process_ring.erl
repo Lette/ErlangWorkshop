a57fdc48154b499b5e246138862b685e9a2dfd60
Christoffer Lette <christoffer.lette@gmail.com>
2016-09-12 23:05:44 +0200

    Added missing recursive calls to loop

--- process_ring.erl
+++ process_ring.erl
@@ -3,11 +3,12 @@
 -export([make/1, send/3]).
 
 make(N) ->
-	spawn(fun() -> init(N) end).
+	spawn(fun() -> init(N - 1) end).
 
 send(Ring, Message, N) ->
 	Ref = make_ref(),
-	Ring ! { send, Ref, self(), Message, N },
+	%%io:format("Calling ~p:send from ~p with data ~p.\n", [Ring, self(), Message]),
+	Ring ! { send, Ref, self(), Message, N - 1 },
 	receive
 		{ complete, Ref, ReceivedMessage } ->
 			"Received " ++ ReceivedMessage ++ ", expected " ++ Message
@@ -28,10 +29,14 @@ init(0, FirstPid) ->
 	loop({ FirstPid }).
 
 loop({ NextProcess }) ->
+	%%io:format("I am ~p. My target is ~p.\n", [self(), NextProcess]),
 	receive
 		{ send, Ref, Client, Message, 0 } ->
-			Client ! { complete, Ref, Message };
+			%%io:format("loop received 'send' with ~p, ~p\n", [Message, 0]),
+			Client ! { complete, Ref, Message },
+			loop({ NextProcess });
 		{ send, Ref, Client, Message, N } ->
-			io:format("~p", [N]),
-			NextProcess ! { send, Ref, Client, Message, N - 1 }
+			%%io:format("loop received 'send' with ~p, ~p\n", [Message, N]),
+			NextProcess ! { send, Ref, Client, Message, N - 1 },
+			loop({ NextProcess })
 	end.
